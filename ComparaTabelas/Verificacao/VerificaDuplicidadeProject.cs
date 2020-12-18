b56c795ed8cf2f79b963f8a1882797a032095637
Luciano Santos <lucianoliv2@hotmail.com>
2020-12-18 14:24:09 -0300

    Add files via upload

---
+++ ComparaTabelas/Verificacao/VerificaDuplicidadeProject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClosedXML.Excel;
+namespace ComparaTabelas
+{
+    class VerificarDuplicidadeProject 
+    {
+
+        private string cellAnterior,cellProxima;
+        private bool achouDuplicidade;
+        public void chamarComparacao()
+        {
+            ComparaAnteriorEProxima();
+        }
+
+        private void iniciaVariavel()
+        {
+            cellAnterior = "";
+            cellProxima = "";
+            achouDuplicidade = false;
+        }
+
+        public void setCellAnterior(string cellAnteriorProject)
+        {
+            iniciaVariavel();
+            cellAnterior = cellAnteriorProject;
+        }
+
+        public void setCellproxima(string cellProximaComparar)
+        {
+            cellProxima = cellProximaComparar;
+        }
+        public bool getAchouduplicidade()
+        {
+            return this.achouDuplicidade; 
+        }
+
+        private void ComparaAnteriorEProxima()
+        {
+            if(cellAnterior==cellProxima)
+            {
+                //Console.ForegroundColor = ConsoleColor.Red;
+                this.achouDuplicidade = true;
+            }
+            else if(cellAnterior != cellProxima)
+            {
+                this.achouDuplicidade = false;
+                //Console.ResetColor();
+            }
+        }
+    }
+}
